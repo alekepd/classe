alekepd/classe

commit 2f6a9106eb656b7ea5e63cb1c05c2828eddcd1b7

Distance function returns now sorted index labels.
This makes the internal ordering labels returned stable and predictable.

src/classe/featurization.py
@@ -747,7 +747,7 @@ def make_distance_table(
 
     distance_array[distance_array > distance_max] = distance_max
     tab = pd.DataFrame(distance_array)
-    dist_names = [ob[0] + "-" + ob[1] for ob in dist_tups]
+    dist_names = [ob[0] + "-" + ob[1] for ob in sorted(dist_tups)]
     tab.columns = dist_names
     return tab
 

commit af1f5a786ee6e97fa3dd8ad383f2931cb1c882dc

Fixed ordering bug in distance calculation, added sorting interface.
Commit 2f6a9106eb656b7ea5e63cb1c05c2828eddcd1b7 introduced a bug and did
not solve the targeted bug. Distances returned via distance function
were incorrect. They are now correct and the intended behavior of
sorting the individual labels in each column name can be performed.

Additionally, this interface is used in the command line tools to have
sorted distance labels in the produced csvs.

examples/tsproj.py
@@ -256,7 +256,11 @@ def get_featurizer(batch_size, distance_max, box_diam):
 
     def f(traj):
         return feat.make_distance_table(
-            traj, batch_size=batch_size, distance_max=distance_max, box_diam=box_diam
+            traj,
+            batch_size=batch_size,
+            distance_max=distance_max,
+            box_diam=box_diam,
+            sort_key=int,
         )
 
     return f

src/classe/featurization.py
@@ -720,7 +720,11 @@ def traj_distances_array(
 
 
 def make_distance_table(
-    pos_array, distance_method=traj_distances_array, distance_max=np.Inf, **kwargs
+    pos_array,
+    distance_method=traj_distances_array,
+    distance_max=np.Inf,
+    sort_key=None,
+    **kwargs
 ):
     """Creates a distance panda from position array.
 
@@ -733,6 +737,12 @@ def make_distance_table(
         more information.
     distance_max:
         Distances larger than distance_max are set to distance max.
+    sort_key (callable):
+        Column names are created by first sorting pairs of strings returned
+        from the underlying distance method. sort_key is passed to `sorted` during
+        this step via the key= argument. If distance_method returns integers
+        cast to strings, set this to `int` to make site labels in each column
+        ordered according to numeric size.
     kwargs:
         Passed to distance_method
 
@@ -747,7 +757,8 @@ def make_distance_table(
 
     distance_array[distance_array > distance_max] = distance_max
     tab = pd.DataFrame(distance_array)
-    dist_names = [ob[0] + "-" + ob[1] for ob in sorted(dist_tups)]
+    dist_tups_sorted = (sorted(x, key=sort_key) for x in dist_tups)
+    dist_names = [ob[0] + "-" + ob[1] for ob in dist_tups_sorted]
     tab.columns = dist_names
     return tab
 